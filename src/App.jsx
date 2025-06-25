f34a1756dc4e7605f14c210b173c36cb57b47b13
BitByBit-101 <bhumikamina96@gmail.com>
2025-06-26 04:24:21 +0530

    Initial commit - portfolio

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/header"
+import HeroSection from "./components/HeroSection"
+import CustomCursor from "./components/CustomCursor"
+export default function App() {
+  return (
+    <>
+    <Header/>
+    <HeroSection/>
+    <CustomCursor/>
+    </>
+  )
+}
